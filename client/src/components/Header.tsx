8f44c9c40871ed20a31da9877aca1c93cf9b53e4
Marcos676 <patino-marcos@outlook.com>
2026-05-02 14:03:49 -0300

    Refactoriza el componente header: - Extrae estructura del nav para reutilizar su estructura segun tamaño de dispositivo. - Agrega logica para despliegue de nav en movile y efectos del boton de modo nocturno

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,101 +1,152 @@
 import logo from "../assets/logo.png";
 import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { replaceClass } from "../utils/handleDOM";
-const Header = () => {
-  //const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [theme, setTheme] = useState("light");
+
+type NavProps = {
+  isMenuOpen: boolean;
+  isMovile: boolean;
+};
+
+const Nav = ({ isMenuOpen, isMovile }: NavProps) => {
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [theme, setTheme] = useState("light"); // Estado para controlar el tema actual (light/dark)
+
+  // captura input checkbox para cambiar su estado al hacer click en el toggle
   const themeRef = useRef<HTMLInputElement>(null);
+  // captura el div del toggle para cambiar su estilo segun el tema
   const toggleThemeRef = useRef<HTMLDivElement>(null);
 
+  // Maneja el cambio de tema al hacer click en el toggle
   const handleTheme = () => {
     if (themeRef.current) {
       themeRef.current.checked = !themeRef.current.checked;
       setTheme(themeRef.current.checked ? "dark" : "light");
     }
   };
+  // Efecto para cambiar el estilo del toggle segun el tema
   useEffect(() => {
     if (toggleThemeRef.current) {
       let sunMoon = toggleThemeRef.current.children[0] as HTMLDivElement;
       if (theme === "dark") {
-        replaceClass(toggleThemeRef.current, "bg-grisClaro", "bg-grisOscuro");
-        replaceClass(toggleThemeRef.current, "justify-start", "justify-end");
+        replaceClass(toggleThemeRef.current, "bg-blue-300", "bg-blue-950");
+        replaceClass(sunMoon, "translate-x-0", "translate-x-full");
         replaceClass(sunMoon, "bg-amarillo", "bg-blanco");
       } else {
-        replaceClass(toggleThemeRef.current, "bg-grisOscuro", "bg-grisClaro");
-        ("bg-grisClaro");
-        replaceClass(toggleThemeRef.current, "justify-end", "justify-start");
+        replaceClass(toggleThemeRef.current, "bg-blue-950", "bg-blue-300");
+        replaceClass(sunMoon, "translate-x-full", "translate-x-0");
         replaceClass(sunMoon, "bg-blanco", "bg-amarillo");
       }
     }
   }, [theme]);
 
+  // Efecto para abrir/cerrar el menú móvil midiendo su altura real para animar su apertura/cierre
+  useEffect(() => {
+    if (!isMovile) return; // Solo aplica este efecto para el menú móvil
+    if (menuRef.current) {
+      // Mide la altura del contenido del menú
+      const height = menuRef.current.scrollHeight + "px";
+      if (isMenuOpen) {
+        menuRef.current.style.height = height;
+      } else {
+        menuRef.current.style.height = "0px";
+      }
+    }
+  }, [isMenuOpen]);
+
+  return (
+    <div
+      ref={menuRef}
+      className={
+        "w-full items-center text-xl overflow-hidden transition-all duration-700 " +
+        (isMovile
+          ? "flex md:hidden flex-col gap-2 h-0"
+          : "hidden md:flex justify-between gap-5")
+      }
+    >
+      <div
+        className={
+          "font-staatliches text-center " +
+          (isMovile ? "" : "bg-grisClaro px-3 py-1 rounded-lg order-last")
+        }
+      >
+        <i className="fa-solid fa-paw"></i> Ingresar
+      </div>
+      <nav
+        className={
+          "text-center " + (isMovile ? "w-full border-t border-b" : "grow")
+        }
+      >
+        <ul
+          className={
+            "flex items-center gap-2 font-staatliches " +
+            (isMovile ? "w-full flex-col" : "justify-between")
+          }
+        >
+          <li className={isMovile ? "w-full" : "grow border-r-2"}>HOME</li>
+          <li className={isMovile ? "w-full" : "grow border-r-2"}>TIENDA</li>
+          <li className={isMovile ? "w-full" : "grow border-r-2"}>NOSOTROS</li>
+          <li className={isMovile ? "w-full" : "grow"}>FAQS</li>
+        </ul>
+      </nav>
+      <div
+        ref={toggleThemeRef}
+        className="w-10 h-5 bg-blue-300 rounded-full cursor-pointer transition-color duration-700"
+        onClick={handleTheme}
+      >
+        <div className="w-5 h-5 rounded-full bg-amarillo transition-transform duration-700 translate-x-0"></div>
+        <input ref={themeRef} type="checkbox" className="hidden" />
+      </div>
+    </div>
+  );
+};
+
+const Header = () => {
+  // Estado para controlar si el menú móvil está abierto o cerrado
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleIsOpenMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="w-full bg-naranjaClaro">
-      <div className="w-full flex flex-wrap flex-col p-2 max-w-3xl mx-auto">
-        <div className="w-full flex items-center gap-2">
-          <a className="h-13 w-13 flex-none" href="/">
+      <div className="w-full flex flex-wrap flex-col p-2 max-w-6xl mx-auto">
+        <div className="w-full flex items-center gap-3">
+          <Link className="h-13 w-13 md:h-21 md:w-21 flex-none" to="/">
             <img
               className="max-w-full h-auto object-contain"
               src={logo}
               alt="logo"
             />
-          </a>
+          </Link>
           <div className="grow flex flex-col gap-4">
-            <div className="w-full flex">
+            <div className="w-full flex gap-4">
               <search className="flex grow bg-white rounded-xl">
                 <input
                   className="px-3 py-1 w-full"
                   type="text"
-                  placeholder="Search..."
+                  placeholder="Busca..."
                 />
                 <button className="border-l px-2 my-1 text-grisOscuro">
                   <i className="fa-solid fa-magnifying-glass"></i>
                 </button>
               </search>
-              <nav className="md:hidden flex-none flex items-center ml-4 text-grisOscuro">
-                <i className="fa-solid fa-bars text-xl"></i>
+              <nav
+                className="md:hidden flex-none flex items-center text-grisOscuro px-1"
+                onClick={handleIsOpenMenu}
+              >
+                <i className="fa-solid fa-bars text-2xl"></i>
               </nav>
-              <div className="flex-none flex items-center ml-4 text-verde">
-                <i className="fa-solid fa-cart-shopping text-lg"></i>
+              <div className="flex-none flex items-center text-verde">
+                <i className="fa-solid fa-cart-shopping text-xl md:text-2xl"></i>
               </div>
             </div>
-            {/* Navegacion > a tablet */}
-            <div className="w-full hidden md:flex ">
-              <nav>
-                <ul className="flex">
-                  <li>HOME</li>
-                  <li>TIENDA</li>
-                  <li>NOSOTROS</li>
-                  <li>FAQS</li>
-                </ul>
-              </nav>
-              <div>Tema</div>
-              <div>Ingresar</div>
-            </div>
+            {/* Navegacion >= a tablet */}
+            <Nav isMenuOpen={isMenuOpen} isMovile={false} />
           </div>
         </div>
         {/* Navegacion desplegable mobile */}
-        <div className="w-full flex flex-col items-center gap-2 text-lg md:hidden">
-          <div className=" font-staatliches border-b text-center px-20">
-            <i className="fa-solid fa-paw"></i> Ingresar
-          </div>
-          <nav className=" border-b text-center px-22">
-            <ul className="flex flex-col items-center gap-2 font-staatliches">
-              <li>HOME</li>
-              <li>TIENDA</li>
-              <li>NOSOTROS</li>
-              <li>FAQS</li>
-            </ul>
-          </nav>
-          <div
-            ref={toggleThemeRef}
-            className="w-10 h-5 flex justify-start items-center bg-grisClaro rounded-full cursor-pointer"
-            onClick={handleTheme}
-          >
-            <div className="w-5 h-5 rounded-full bg-amarillo"></div>
-            <input ref={themeRef} type="checkbox" className="hidden" />
-          </div>
-        </div>
+        <Nav isMenuOpen={isMenuOpen} isMovile={true} />
       </div>
     </header>
   );
