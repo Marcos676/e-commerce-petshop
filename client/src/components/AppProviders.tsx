e791e1849b1070b66d093f04f1d5cb7a24ad5c34
Marcos676 <patino-marcos@outlook.com>
2026-05-01 21:14:26 -0300

    Instala react-router-dom y lo configura en AppProviders.tsx

--- client/src/components/AppProviders.tsx
+++ client/src/components/AppProviders.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { BrowserRouter } from "react-router-dom";
+
 import App from './App.tsx'
 
 function AppProviders() {
 
   return (
     <>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </>
   )
 }
