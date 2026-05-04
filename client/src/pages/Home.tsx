ad83883eeae8aff68bba664320453e2a0b496a46
Marcos676 <patino-marcos@outlook.com>
2026-05-04 20:44:55 -0300

    Refactoriza el componente Home: Crea ciclos para manejar codigo repetido como estructuras de lista y componentes de carrusel. Completa el diseño de la seccion "Busca por tipo de mascota" con Grid

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,9 +4,9 @@ import { Autoplay } from "swiper/modules"; //importa modulo de autoplay para que
 import "swiper/css"; // Importa estilos
 import "swiper/css/autoplay";
 
-//type Props = {}
 
-const Home = (/* props: Props */) => {
+
+const Home = () => {
   return (
     <>
       {/* carrusel */}
@@ -16,47 +16,40 @@ const Home = (/* props: Props */) => {
         loop={true} //Se repite infinitamente
         slidesPerView={1} //Muestra un slide a la vez
       >
-        <SwiperSlide>
-          <img
-            src="images/carrusel/carrusel1.jpg"
-            className="object-contain"
-            alt=""
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="images/carrusel/carrusel2.jpg"
-            className="object-contain"
-            alt=""
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/carrusel/carrusel3.jpg" className="" alt="" />
-        </SwiperSlide>
+        {["carrusel1.jpg", "carrusel2.jpg", "carrusel3.jpg"].map(
+          (img, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={`images/carrusel/${img}`}
+                className="object-contain"
+                alt="Imagen de banner"
+              />
+            </SwiperSlide>
+          ),
+        )}
       </Swiper>
-      <section>
-        <h3>Busca por tipo de mascota</h3>
-        <ul>
-          <li>
-            <img src="images/categorias/iconoPerro.png" alt="Perro" />
-          </li>
-          <li>
-            <img src="images/categorias/iconoGato.png" alt="Gato" />
-          </li>
-          <li>
-            <img src="images/categorias/iconoPez.png" alt="Pez" />
-          </li>
-          <li>
-            <img src="images/categorias/iconoRoedor.png" alt="Roedor" />
-          </li>
-          <li>
-            <img src="images/categorias/iconoAve.png" alt="Ave" />
-          </li>
-          <li>
-            <img src="images/categorias/iconoReptil.png" alt="Reptil" />
-          </li>
+      <section className="px-2 my-4">
+        <h2 className="mb-2 text-2xl text-grisOscuro">
+          Busca por tipo de mascota
+        </h2>
+        <ul
+          className="grid justify-around items-center"
+          style={{ gridTemplateColumns: "repeat(auto-fit, 100px)" }}
+        >
+          {["Perro", "Gato", "Pez", "Roedor", "Ave", "Reptil"].map(
+            (tipo, index) => (
+              <li key={index} className="mx-auto">
+                <img
+                  className="max-w-full h-auto object-contain"
+                  src={`images/categorias/icono${tipo}.png`}
+                  alt={tipo}
+                />
+              </li>
+            ),
+          )}
         </ul>
       </section>
+      
     </>
   );
 };
